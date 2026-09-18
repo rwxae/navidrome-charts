d99467c62b6ec95cf13b995041df50d3ce0d57cd
wardxela <wardxela@gmail.com>
2026-09-18 13:10:29 +0300

    Add "Recently Played" playlist

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,10 @@ struct Args {
     /// Most Played playlist title
     #[arg(long)]
     most_played_title: Option<String>,
+
+    /// Recently Played playlist title
+    #[arg(long)]
+    recently_played_title: Option<String>,
 }
 
 fn create_or_update_most_played(
@@ -65,6 +69,41 @@ fn create_or_update_most_played(
     Ok(())
 }
 
+fn create_or_update_recently_played(
+    db: &Connection,
+    client: &SubsonicClient,
+    title: String,
+    max_size: u32,
+    playlists: &[Playlist],
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut statement = db.prepare(
+        "
+            SELECT
+                mf.id,
+                MAX(a.play_date) AS play_date
+            FROM media_file mf
+            JOIN annotation a ON mf.id = a.item_id AND a.item_type = 'media_file'
+            WHERE a.play_date IS NOT NULL
+            GROUP BY mf.id
+            ORDER BY play_date DESC
+            LIMIT ?1;
+        ",
+    )?;
+
+    let songs = statement
+        .query_map([max_size], |row| row.get(0) as Result<String, _>)?
+        .map(|song| song.unwrap());
+
+    let maybe_playlist_id = playlists
+        .iter()
+        .find(|&playlist| playlist.name == title && playlist.owner == client.username())
+        .map(|playlist| playlist.id.clone());
+
+    client.create_playlist(title, songs, maybe_playlist_id)?;
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let Args {
         db_path,
@@ -73,6 +112,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         password,
         max_size,
         most_played_title,
+        recently_played_title,
     } = Args::parse();
 
     let db_connection = Connection::open_with_flags(db_path, OpenFlags::SQLITE_OPEN_READ_ONLY)?;
@@ -86,6 +126,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &db_connection,
         &subsonic_client,
         most_played_title,
+        max_size,
+        &playlists,
+    )?;
+
+    let recently_played_title =
+        recently_played_title.unwrap_or_else(|| format!("{} Recently Played - Global", max_size));
+    create_or_update_recently_played(
+        &db_connection,
+        &subsonic_client,
+        recently_played_title,
         max_size,
         &playlists,
     )?;
